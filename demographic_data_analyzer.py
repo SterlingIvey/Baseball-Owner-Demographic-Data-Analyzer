ff1f5cf1039165a16b20de8546cd41885f3ee827
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-06 05:29:39 -0700

    Update demographic_data_analyzer.py
    Add changes from VS Code

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,49 +1,59 @@
 import pandas as pd
 
-
 def calculate_baseball_owners_data(print_data=True):
     # Read data from file
-    df = pd.read_csv('baseball_owners_data.csv')
-    
-    # Data cleaning and preprocessing
-    df['net_worth'] = df['net_worth'].replace('[\$,]', '', regex=True).astype(float)
-    df['franchise_value'] = df['franchise_value'].replace('[\$,]', '', regex=True).astype(float)
+    df = pd.read_csv('baseball_owners_data')
 
     # How many of each race are represented in this dataset of baseball owners?
     race_count = df['race'].value_counts()
 
     # What is the average age of baseball owners?
-    average_age_owner = round(df['age'].mean(), 1)
+    average_age_owners = round(df['age'].mean(), 1)
 
-    # What percentage of baseball owners have a Bachelor's degree?
-    percentage_bachelors = round(df.loc[df['education'] == 'Bachelors', 'education'].count() / df.shape[0] * 100,1)
+    # What about median age?
+    median_age_owners = round(df['age'].median(), 1)
+
+    # What percentage of baseball owners have at least a Bachelor's degree?
+    percentage_bachelors = round(df.loc[df['education'] == 'bachelors', 'education'].count() / df.shape[0] * 100,1)
     
     # What is the most popular industry among baseball owners?
     top_industry = df['industry'].value_counts().idxmax()
     
-    # What is the average net worth of baseball owners?
-    average_net_worth = df['net_worth'].mean()
+    # What is the median net worth of baseball owners?
+    median_net_worth = df['net_worth'].median()
     
     # What is the average value of the teams owned?
-    df['franchise_value'] = df['franchise_value'].replace('[\$,]', '', regex=True).astype(float)
     average_team_value = df['franchise_value'].mean()
 
+    max_value_index = df['franchise_value'].idxmax()
 
-    # UI Output below here 
+    # What is the most valuable franchise?
+    most_valuable_franchise_row = df.loc[df['franchise_value'].idxmax()]
+
+    # Extract the team name and franchise value from the row
+    most_valuable_team = most_valuable_franchise_row['team']
+    most_valuable_value = most_valuable_franchise_row['franchise_value']
+
+    # Data output is below this line
 
     if print_data:
         print("Number of each race:\n", race_count) 
-        print("Average age of owner:", average_age_owner)
-        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
-        print("Most popular industry among owners:", top_industry)
-        print("Average net worth of owners: ${:,.2f}".format(average_net_worth))
-        print("Average value of teams: ${:,.2f}".format(average_team_value))
+        print("Current Average Age of Owners:", average_age_owners)
+        print("Current Median Age of Owners:", median_age_owners)
+        print(f"Current Percentage with at least Bachelors degrees: {percentage_bachelors}%")
+        print("Current Most popular industry among owners:", top_industry)
+        print("Current Median net worth of owners: ${:,.2f}".format(median_net_worth))
+        print("Current Average value of teams: ${:,.2f}".format(average_team_value))
+        print("Most Valuable Franchise: {most_valuable_team} with a value of ${most_valuable_value:} ")
 
     return {
         'race_count': race_count,
-        'average_age_owner': average_age_owner,
+        'average_age_owners': average_age_owners,
+        'median_age_owners': median_age_owners,
         'percentage_bachelors': percentage_bachelors,
         'top_industry': top_industry,
-        'average_net_worth': average_net_worth,
-        'average_team_value': average_team_value
+        'median_net_worth': median_net_worth,
+        'average_team_value': average_team_value,
+        'most_valuable_franchise': most_valuable_team
     }
+    return calculate_baseball_owners_data()
