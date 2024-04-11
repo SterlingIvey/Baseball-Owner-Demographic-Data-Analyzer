9987e91d970a36796b5980d2055a93100b912aa3
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-11 08:58:35 -0400

    Update tbr.py

--- tbd/tbr.py
+++ tbd/tbr.py
@@ -15,4 +15,4 @@ num_years = df['year'].iloc[-1] - df['year'].iloc[0]
 annualized_return = ((ending_value / beginning_value) ** (1 / num_years)) - 1
 annualized_return_percentage = annualized_return * 100
 
-print(f
+print(f"Annualized return since 2002: {annualized_return_percentage:.2f}%")
