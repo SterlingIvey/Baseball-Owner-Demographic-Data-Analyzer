f23090cd3833c1ba963068a59ea5efd9a78db7ad
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-11 10:27:30 -0400

    Update app.py

--- which_team/app.py
+++ which_team/app.py
@@ -1,3 +1,9 @@
 import pandas as pd
 
+def get_team_recommendation(): 
+    print("Welcome User! Which baseball team should you root for?")
+    
+    # Begin questions
+    questions = [
+
 teams_data = {"Team": ["New York Mets", "Atlanta Braves", "Miami Marlins", "Philadelphia Phillies", "Washington Nationals", "Chicago Cubs", "Cincinnati Reds", "Milwaukee Brewers", "Pittsburgh Pirates", "St Louis Cardinals", "Arizona Diamondbacks", "Colorado Rockies", "San Diego Padres", "San Francisco Giants", "Baltimore Orioles", "New York Yankees", "Tampa Bay Rays", "Boston Red Sox", 
