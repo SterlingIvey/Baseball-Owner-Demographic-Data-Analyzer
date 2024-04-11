8c97d3b88cf2c5e7a0bdbda3b938aa184f248b10
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-11 10:12:33 -0400

    Update app.py

--- which_team/app.py
+++ which_team/app.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-teams_data = {"Team": ["Atlanta Braves", 
+teams_data = {"Team": ["New York Mets", "Atlanta Braves", "Miami Marlins", "Philadelphia Phillies", "Washington Nationals", "Chicago Cubs", "Cincinnati Reds", "Milwaukee Brewers", "Pittsburgh Pirates", 
