5ad2463eec8ec63d031f4d8017aa6b26aa2c17b5
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-11 10:19:49 -0400

    Update app.py

--- which_team/app.py
+++ which_team/app.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-teams_data = {"Team": ["New York Mets", "Atlanta Braves", "Miami Marlins", "Philadelphia Phillies", "Washington Nationals", "Chicago Cubs", "Cincinnati Reds", "Milwaukee Brewers", "Pittsburgh Pirates", "St Louis Cardinals", "Arizona Diamondbacks", "Colorado Rockies", "San Diego Padres", "San Francisco Giants", "Baltimore Orioles", "New York Yankees", "Tampa Bay Rays",
+teams_data = {"Team": ["New York Mets", "Atlanta Braves", "Miami Marlins", "Philadelphia Phillies", "Washington Nationals", "Chicago Cubs", "Cincinnati Reds", "Milwaukee Brewers", "Pittsburgh Pirates", "St Louis Cardinals", "Arizona Diamondbacks", "Colorado Rockies", "San Diego Padres", "San Francisco Giants", "Baltimore Orioles", "New York Yankees", "Tampa Bay Rays", "Boston Red Sox", 
