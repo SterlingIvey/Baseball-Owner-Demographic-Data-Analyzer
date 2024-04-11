3e23a24751056f685f2c41f72b5107364e07f96c
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-11 11:59:26 -0400

    Update app.py

--- which_team/app.py
+++ which_team/app.py
@@ -6,4 +6,10 @@ def get_team_recommendation():
     # Begin questions
     questions = [{"question": "Are you a history buff?"}, {"question": "Where do you live?"}, {"question": "Are you passionate about other sports?"}, {"question": "Big drinker?"}, {"question": "Did you grow up playing baseball?"}, {"question": "Do you want to be able to own a piece of your team?"}]
     # Team data and weightings
-    teams_data = {"Team": ["New York Mets": {"history": 2}, "Atlanta Braves": {"history": 1}, "Miami Marlins": {"history": 2}, "Philadelphia Phillies": {"history": 1}, "Washington Nationals": {"history": 2}, "Chicago Cubs": {"history": 1}, "Cincinnati Reds": {"history": 1}, "Milwaukee Brewers": {"history": 1}, "Pittsburgh Pirates": {"history": 1}, "St Louis Cardinals": {"history": 1}, "Arizona Diamondbacks": {"history": 2}, "Colorado Rockies": {"history": 2}, "San Diego Padres": {"history": 2}, "San Francisco Giants": {"history": 1}, "Baltimore Orioles": {"history": 1}, "New York Yankees": {"history": 1}, "Tampa Bay Rays": {"history": 2}, "Boston Red Sox": {"history": 1}, "New York Yankees": {"history": 1}, "Tampa Bay Rays": {"history": 2}, "Toronto Blue Jays": {
+    teams_data = {"Team": ["New York Mets": {"history": 2}, 
+                           "Atlanta Braves": {"history": 1}, 
+                           "Miami Marlins": {"history": 2}, 
+                           "Philadelphia Phillies": {"history": 1}, 
+                           "Washington Nationals": {"history": 2}, 
+                           "Chicago Cubs": {"history": 1}, 
+                           "Cincinnati Reds": {"history": 1}, "Milwaukee Brewers": {"history": 1}, "Pittsburgh Pirates": {"history": 1}, "St Louis Cardinals": {"history": 1}, "Arizona Diamondbacks": {"history": 2}, "Colorado Rockies": {"history": 2}, "San Diego Padres": {"history": 2}, "San Francisco Giants": {"history": 1}, "Baltimore Orioles": {"history": 1}, "New York Yankees": {"history": 1}, "Tampa Bay Rays": {"history": 2}, "Boston Red Sox": {"history": 1}, "New York Yankees": {"history": 1}, "Tampa Bay Rays": {"history": 2}, "Toronto Blue Jays": {
