ef8d4b11db453ace3baf8329c2ed3f13874741f0
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-11 08:32:49 -0400

    Update tbr.py

--- tbd/tbr.py
+++ tbd/tbr.py
@@ -12,3 +12,4 @@ plt.show()
 beginning_value = df['franchise_value'].iloc[0]
 ending_value = df['franchise_value'].iloc[1]
 num_years = df['year'].iloc[-1] - df['year'].iloc[0]
+annualized_return = ((ending_value / beginning_value) ** (1 / num_years)) - 1
